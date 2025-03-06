51e14fe9e4d61ff75877625c84fa8f664508ac90
Zainab 27 <zainabalsaffar03@gmail.com>
2025-03-06 12:15:29 +0300

    Allegedly finished the task

--- src/variables.ts
+++ src/variables.ts
@@ -9,11 +9,12 @@
  * 5. image         -> assign it a value of a url of your image or ant image that represents you online
  */
 
-let fullName: string;
-let yearOfBirth: number;
-let hobby: string;
-let funFact: string;
-let image: string;
+let fullName: string = "Zainab AlSaffar";
+let yearOfBirth: number = 2003;
+let hobby: string = "Photography";
+let funFact: string = "I once went to the cinema 10 times in one month!";
+let image: string =
+  "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.capitalccg.ac.uk%2Fnews%2Fhow-to-become-a-software-engineer&psig=AOvVaw3CYe97skUvKRnh3wYpzL6J&ust=1741337398354000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCNiw1L2J9YsDFQAAAAAdAAAAABAE";
 
 /**
  * Part 2: String Interpolation
@@ -25,9 +26,9 @@ let image: string;
  * 3. hobbyString         -> assign it: "My hobby is {YOUR_HOBBY}""
  */
 
-let fullNameString: string;
-let ageString: string;
-let hobbyString: string;
+let fullNameString: string = "My name is ${fullName}";
+let ageString: string = "I am ${2025 - yearOfBirth}";
+let hobbyString: string = "My hobby is ${hobby}";
 
 /**
  * Part 3: Re-assignment
@@ -38,16 +39,20 @@ let hackerScore = 0;
 
 function incrementBy1() {
   // Increment hackerScore by 1 👇🏻
+  hackerScore = hackerScore + 1;
 }
 function decrementBy1() {
   // decrement hackerScore by 1 👇🏻
+  hackerScore = hackerScore - 1;
 }
 
 function incrementBy2() {
   // Increment hackerScore by 2 👇🏻
+  hackerScore = hackerScore + 2;
 }
 function decrementBy2() {
   // decrement hackerScore by 2 👇🏻
+  hackerScore = hackerScore - 2;
 }
 
 // Ignore this part (:

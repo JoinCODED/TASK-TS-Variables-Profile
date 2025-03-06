36c4e1424ee8a74c6b3bf35c6779cf96224d05c2
MishariAldoub <MEAldoub@pifss.gov.kw>
2025-03-06 12:26:25 +0300

    finished tasks

--- src/variables.ts
+++ src/variables.ts
@@ -17,7 +17,7 @@ let image: string;
 
 fullName = "Misahri Aldoub"
 yearOfBirth = 1996
-hobby = "3d Printing and 3d Design!"
+hobby = "3d Printing and 3d Design"
 funFact = "I have a Lorikeet bird who loves the word BAS"
 image =  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRozK1Bw-BYXK53WRrRTMeSSdUkQ4N8KEaLsw&s"
 
@@ -35,8 +35,8 @@ let fullNameString: string;
 let ageString: string;
 let hobbyString: string;
 fullNameString = `My name is ${fullName}`
-ageString = `I am ${yearOfBirth}!`
-hobby = `My hobby is ${hobby}`
+ageString = `I am ${2025 - yearOfBirth }`
+hobbyString = `My hobby is ${hobby}`
 /**
  * Part 3: Re-assignment
  * Increment your hacker score
@@ -46,16 +46,21 @@ let hackerScore = 0;
 
 function incrementBy1() {
   // Increment hackerScore by 1 👇🏻
+hackerScore++
+
 }
 function decrementBy1() {
   // decrement hackerScore by 1 👇🏻
+  hackerScore--
 }
 
 function incrementBy2() {
   // Increment hackerScore by 2 👇🏻
+  hackerScore += 2
 }
 function decrementBy2() {
   // decrement hackerScore by 2 👇🏻
+  hackerScore -=2
 }
 
 // Ignore this part (:

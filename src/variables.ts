e15913533b112b1044d7bd26efab80412b764014
A-M98 <aalmohammad@pifss.gov.kw>
2025-03-06 11:49:31 +0300

    Ahmed Al-Mohammed

--- src/variables.ts
+++ src/variables.ts
@@ -9,11 +9,12 @@
  * 5. image         -> assign it a value of a url of your image or ant image that represents you online
  */
 
-let fullName: string;
-let yearOfBirth: number;
-let hobby: string;
-let funFact: string;
-let image: string;
+let fullName: string = "Ahmed Al-Mohammed";
+let yearOfBirth: number = 10 / 1 / 1998;
+let hobby: string = "Riding dirtbikes and mechanical work";
+let funFact: string = " I love mangos";
+let image: string =
+  "https://s3.us-east-2.amazonaws.com/prod.mm.com/img/articles/Lead1_VHaUGFh.jpeg";
 
 /**
  * Part 2: String Interpolation
@@ -25,9 +26,9 @@ let image: string;
  * 3. hobbyString         -> assign it: "My hobby is {YOUR_HOBBY}""
  */
 
-let fullNameString: string;
-let ageString: string;
-let hobbyString: string;
+let fullNameString: string = `My name is ${fullName}`;
+let ageString: string = `I am ${Date.now() - yearOfBirth}`;
+let hobbyString: string = `My hobby is ${hobby}`;
 
 /**
  * Part 3: Re-assignment
@@ -38,16 +39,20 @@ let hackerScore = 0;
 
 function incrementBy1() {
   // Increment hackerScore by 1 👇🏻
+  hackerScore = hackerScore + 1;
 }
 function decrementBy1() {
   // decrement hackerScore by 1 👇🏻
+  hackerScore = hackerScore - 1;
 }
 
 function incrementBy2() {
   // Increment hackerScore by 2 👇🏻
+  hackerScore = hackerScore + 2;
 }
 function decrementBy2() {
   // decrement hackerScore by 2 👇🏻
+  hackerScore = hackerScore - 2;
 }
 
 // Ignore this part (:
